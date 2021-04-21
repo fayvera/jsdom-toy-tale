cdbfb04dc6c874ad49331a4160a06c15aee60e2a
Fay Vera <fvera.cavalcante@gmail.com>
2021-04-20 22:16:24 -0400

    commit tree ecad4ce5ae97df1c0c4fbbfa127874edb9eed341

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 let addToy = false;
 
+const addBtn = document.querySelector('#new-toy-btn')
+const toyForm = document.querySelector('.container')
+let divCollect = document.querySelector('#toy-collection')
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
@@ -13,3 +19,99 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+function getToys(){
+  fetch("http://localhost:3000/toys")
+  .then(res => res.json())
+}
+
+
+function postToy(toyData) {
+  fetch("http://localhost:3000/toys", {
+    method: "POST", 
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+      "name": toy_data.name.value,
+      "image": toy_data.image.value,
+      "likes": 0
+    })
+  })
+  .then(res => res.json())
+  .then(obj => {
+    renderToys(obj)
+  })
+
+}
+
+function likes(e){
+  e.preventDefault()
+  let more = parseInt(e.target.previousElementSibling.innerText) + 1
+
+  fetch(`http://localhost:3000/toys/${e.target.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+
+      },
+      body: JSON.stringify({
+        "likes": more
+      })
+    })
+    .then(res => res.json())
+    .then((like_obj => {
+      e.target.previousElementSibling.innerText = `${more} likes`;
+    }))
+}
+
+function renderToys(obj) {
+  let h2 = document.createElement("h2")
+  h2.innerText = obj.name
+
+  let img = document.createElement("img")
+  img.setAttribute('src', obj.image) 
+  img.setAttribute('class', "toy-avatar") 
+
+  let p = document.createElement('p')
+  p.innerText = `${obj.likes} likes`
+
+  let btn = document.createElement('button')
+  btn.setAttribute('class', 'like-btn')
+  btn.setAttribute('id', obj.id)
+  btn.innerText = "like"
+  btn.addEventListener('click', function(e){
+      console.log(e);
+      likes(e)
+  })
+  let divCard = document.createElement('div')
+  divCard.setAttribute('class', 'card')
+  divCard.append(h2, img, p, btn)
+  divCollect.append(divCard)
+}
+
+
+
+// addBtn.addEventListener('click', () => {
+//   // hide & seek with the form
+//   addToy = !addToy
+//   if (addToy) {
+//     toyForm.style.display = 'block'
+//     toyForm.addEventListener('submit', event => {
+//       event.preventDefault()
+//       postToy(event.target)
+//     })
+//   } else {
+//     toyForm.style.display = 'none'
+//   }
+// })
+
+// start by getting all toys
+
+getToys().then(toys => {
+  toys.forEach(toy => {
+    //function to render toys goes here or something
+    renderToys(toy)
+  })
+})
